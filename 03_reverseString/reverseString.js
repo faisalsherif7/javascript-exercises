202e20ddf2ae04b994e4da3c72f31c0a3d8f41e5
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-06-23 23:46:59 +0530

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = Array.from(word);
+    let result = '';
+    for (let i = (word.length - 1); i >= 0; i--) {
+        result += wordArray[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
