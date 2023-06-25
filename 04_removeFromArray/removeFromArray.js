9869570e7535ac44f626e493264608d8038ea619
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-06-25 11:56:23 +0530

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
 
+    let array = arguments[0];
+
+    for (let j = 1; j <= arguments.length; j++) {
+        let currentArgument = arguments[j];
+        for (let i = 0; i < array.length; i++) {
+            let currentItem = array[i];
+            if (currentItem === currentArgument) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
