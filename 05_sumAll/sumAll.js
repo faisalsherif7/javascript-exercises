7e1ff1bba7c2c2f05cc0d314c3998373187dea9b
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-06-25 12:06:48 +0530

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0 || typeof(number1) != 'number' || typeof(number2) != 'number') {
+        return "ERROR"
+    } 
+    let firstInteger = 0;
+    let secondInteger = 0;
+    if (number1 < number2) {
+        firstInteger = number1;
+        secondInteger = number2;
+    }
+    else {
+        firstInteger = number2;
+        secondInteger = number1;
+    }
+    let sum = 0;
+    for (let i = firstInteger; i <= secondInteger; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
